7274de23ca88d1010bf3074293b51e7426bb56b1
Berk Ozer <berkozer@Berks-MacBook-Pro.local>
2020-03-15 15:22:16 -0400

    Add useState hook for todos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(
+    [
+      {text: 'Learn to make a react to-do app'},
+      {text: 'get takeout for dinner'},
+      {text: 'shave and go to bed'}
+    ]
+  )
+
   return (
     <div className="App">
       <header className="App-header">
