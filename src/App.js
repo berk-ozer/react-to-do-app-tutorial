6333e0ecb19649a3b4d0f13e18e7b453c07c7b62
Berk Ozer <galipberkozer@gmail.com>
2020-03-15 16:00:30 -0400

    Add TodoForm component to let users create new todo items

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ const Todo = ({ todo }) => {
   )
 }
 
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!value) {
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState(
     [
