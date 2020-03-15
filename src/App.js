7a4b259d2fb5e6f481f254a691288b66c278da5f
Berk Ozer <galipberkozer@gmail.com>
2020-03-15 15:39:34 -0400

    Render a list of Todo components from todos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const ToDo = ({ todo }) => {
+const Todo = ({ todo }) => {
   return (
   <div className="todo">{todo.text}</div>
   )
@@ -12,27 +12,22 @@ function App() {
   const [todos, setTodos] = useState(
     [
       {text: 'Learn to make a react to-do app'},
-      {text: 'get takeout for dinner'},
-      {text: 'shave and go to bed'}
+      {text: 'Get takeout for dinner'},
+      {text: 'Shave and go to bed'}
     ]
   )
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
